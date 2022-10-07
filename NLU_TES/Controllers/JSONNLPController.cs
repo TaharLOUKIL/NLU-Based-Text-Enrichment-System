a8e37fa80e2d3c8fe54a5192a9ad24e7c7f124b3
PC LOUKIL <loukil.tahar@gmail.com>
2022-10-07 21:28:10 +0200

    Ajout de controlleur JSONNLP

---
+++ NLU_TES/Controllers/JSONNLPController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NLU_Aggregator.Controllers
+{
+    public class JSONNLPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
