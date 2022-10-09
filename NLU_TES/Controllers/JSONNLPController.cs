0f17bc3bb4d9e491a5d86ab8a2e236625dede5c4
PC LOUKIL <loukil.tahar@gmail.com>
2022-10-09 15:34:58 +0200

    Appel de Botpress NLU en production pour l'extraction des intention et similarités

--- NLU_TES/Controllers/JSONNLPController.cs
+++ NLU_TES/Controllers/JSONNLPController.cs
@@ -1,12 +1,102 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using NLU_Aggregator.Models;
+using Ocelot.RequestId;
+using Ocelot.Responses;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 
 namespace NLU_Aggregator.Controllers
 {
     public class JSONNLPController : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Méthode qui se charge de l'appel du borpress NLU pour la détermination des intentions et similarités
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public JsonResult Index(string data)
         {
-            return View();
+
+            // 1. Instanciation du modèle principal
+            JSONNLP jSONNLP = new JSONNLP();
+            jSONNLP.Title = data; 
+
+
+            // 2. Connexion à la WEBAPI BOTPRESS Datasphera en Production
+
+            string UrlAuth = "https://automation.datasphera.com/api/v1/auth/login/basic/default";
+            HttpClient httpClient = new HttpClient();
+            var headers = new Dictionary<string, string>();
+            string email = "contact@datasphera.com";
+            string password = "Ex@cctly123";
+            headers.Add("email", email);
+            headers.Add("password", password);
+
+            
+            var response = httpClient.PostAsJsonAsync(UrlAuth, headers).Result.Content.ReadAsStringAsync().Result;
+            var jsonToken = JObject.Parse(response);
+
+
+            // 3. extraction des intentions et similarités
+            
+            
+            string UrlIntents = "automation.datasphera.com/api/v1/bots/test-bot-one/converse/123456/secured?include=nlu";
+            headers.Clear(); // remise à zéro des paramètres
+            headers.Add("Content-Type", "application/json");
+            headers.Add("Authorization", jsonToken.ToString());
+
+           
+
+
+
+            JsonResult resultat = new JsonResult(""); // à remplacer par le Json en résultat
+            response = httpClient.PostAsJsonAsync(UrlIntents, headers).Result.Content.ReadAsStringAsync().Result;
+            
+            
+            var JsonNlpEnrichi = JObject.Parse(response);
+
+            // Appel de la BotPress API avec une phrase
+            string jsonData = ""; // a mettre le retour de l'api
+
+            if (JsonValidate(jsonData))
+            {
+                //appel de la 
+            }
+
+            //Appel de Spacy
+
+
+
+
+
+            // Renvoyer la 
+
+            return resultat;
+           
+        }
+        /// <summary>
+        /// Validation du Schema JSON NLP
+        /// </summary>
+        /// <returns></returns>
+        Boolean JsonValidate(string JsonData) 
+        {
+            // extraction du shcema Json du fichier JSON-NLP.schema.json  pour la vlaidation // https://www.jsonschemavalidator.net/
+
+            var JsonSchemaFile = Path.Combine("JSON-NLP.schema.json");
+            var r = new StreamReader(JsonSchemaFile);
+            var myJsonSchema = r.ReadToEnd();
+            JSchema schema = JSchema.Parse(@myJsonSchema);
+
+            // Vérification de la data fournie.
+            JObject JsonNlp = JObject.Parse(JsonData);
+
+            bool valid = JsonNlp.IsValid(schema);
+            return valid;    
         }
     }
 }
--- NLU_TES/Program.cs
+++ NLU_TES/Program.cs
@@ -1,13 +1,42 @@
+
+using Newtonsoft.Json.Linq;
 using NLU_Aggregator;
+using System.Net.Http;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        //CreateHostBuilder(args).Build().Run();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped(hc => new HttpClient { BaseAddress = new Uri("https://automation.datasphera.com/") });
+        var app = builder.Build();
+
+        // Configuration du Http request pipeline
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        var headers = new Dictionary<string, string>();
+        headers.Add("email", "contact@datasphera.com");
+        headers.Add("password", "Ex@cctly123");
+        
+        
+       /* app.MapGet("explicit-http-client", async(HttpClient httpClient) => {
+            //return await httpClient.PostAsJsonAsync("api/v1/auth/login/basic/default", headers).Result.ToString();//PosFromJsonAsync<List<Token>>("api/v1/auth/login/basic/default");
+            var response = httpClient.PostAsJsonAsync("https://automation.datasphera.com/api/v1/auth/login/basic/default", headers).Result.Content.ReadAsStringAsync().Result;
+            var json = JObject.Parse(response);
+        });*/
+           
+        
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+   /* public static IHostBuilder CreateHostBuilder(string[] args) =>
    Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
@@ -17,7 +46,7 @@ public class Program
                         opt.ValidateOnBuild = false;
                     });
                 })
-            .ConfigureAppConfiguration(config => config.AddJsonFile("ocelot.json"));
+            .ConfigureAppConfiguration(config => config.AddJsonFile("ocelot.json"));*/
 
 }
 
