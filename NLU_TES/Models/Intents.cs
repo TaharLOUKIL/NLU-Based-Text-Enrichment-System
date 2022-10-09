74f63ae0be24de69f8457e52481719eab2408357
PC LOUKIL <loukil.tahar@gmail.com>
2022-10-09 15:43:22 +0200

    Ajout du modèle des intentions

---
+++ NLU_TES/Models/Intents.cs
@@ -0,0 +1,10 @@
+﻿namespace NLU_Aggregator.Models
+{
+    public class Intents
+    {
+        public string? Name { get; set; }
+        public string? Context { get; set; }
+        public string? Confidence { get; set; }
+
+    }
+}
---
+++ NLU_TES/Models/JsonNlpEnrichi.cs
@@ -0,0 +1,13 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+using Newtonsoft.Json;
+using NLU_Aggregator.Models;
+
+namespace NLU_Aggregator.Models
+{
+    public class JsonNlpEnrichi  : JSONNLP
+    {
+        public Intents? intents { get; set; }
+    }
+
+
+}
